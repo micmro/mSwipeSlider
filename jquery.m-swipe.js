0ab8da834ddbfc9a9be2b45fb8251cac8095a604
Michael Mrowetz <me@michaelmrowetz.com>
2014-03-04 23:43:35 -0500

    animation and transition added

--- jquery.m-swipe.js
+++ jquery.m-swipe.js
@@ -1,5 +1,5 @@
 /*Slider plugin*/
-(function($){
+(function($, Modernizr){
 	"use strict";
 
 	$.fn.mSwipe = function( options ) {
@@ -8,20 +8,28 @@
 			onTransitionStart : function() {},
 			onTransitionEnd : function() {},
 			onFinishedSetup : function() {},
-			duration : 500
+			duration : 500,
+			supportsCsstransitions : !!(Modernizr||{}).csstransitions
 		}, options );
 
 		return this.each(function(i, el) {
-			var $this = $(el),
-				$slideWrap = $this.children(".mSwipe-wrap"),
-				$slides = $slideWrap.children("li"),
-				$next = $this.closest(".mSwipe-next"),
-				$prev = $this.closest(".mSwipe-prev");
+			var self = this,
+				$this = $(el),
+				$slideSled = $this.children(".mSwipe-sled"),
+				$slides = $slideSled.children("li"),
+				$next = $this.find(".mSwipe-next"),
+				$prev = $this.find(".mSwipe-prev");
 
-			var currSlide = 1,
-				totalSlides = $slides.length;
+			//temp debug
+			window._this = $this;
+			window._next = $next;
+			window._prev = $prev;
 
-			var initDimensions = function(){
+			var activeSlide = 0,
+				totalSlides = $slides.length-1;
+
+			self.initDimensions = function(){
+				console.log("initDimensions");
 				//get highest element height
 				var maxHeight = Math.max.apply(null, $slides.map(function (){
 						return $(this).height();
@@ -33,64 +41,105 @@
 					width: outerWidth,
 					height: maxHeight
 				});
-				$slideWrap.css({
-					width: outerWidth * totalSlides + "px",
-					height: maxHeight
+				$slideSled.css({
+					width: outerWidth * (totalSlides+1) + "px",
+					height: maxHeight,
+					left : -outerWidth * activeSlide
 				});
+			};
+
+
+			var throttleAndCleanup = function throttle(fn, cleanUpFn) {
+				var last, deferTimer, deferCleanupTimer;
+
+				var resetCleanup = function(){
+					clearTimeout(deferTimer);
+					deferCleanupTimer = setTimeout(function(){
+						cleanUpFn.apply(self);
+					}, 50);
+				};
+
+				return function () {
+					var now = +new Date, args = arguments;
+					if (last && now < last + 50) {
+						clearTimeout(deferTimer);
+						clearTimeout(deferCleanupTimer);
+						deferTimer = setTimeout(function () {
+							last = now;
+							$slideSled.addClass("noTrans");
+							fn.apply(self, args);
+							resetCleanup();
+						}, 50);
+					} else {
+						last = now;
+						$slideSled.addClass("noTrans");
+						fn.apply(self, args);
+						resetCleanup();
+					}
+				};
 			};
 
 
 			var bindEvents = function(){
-				$this.on("mousedown", ".mSwipe-next", methods.next);
-				$this.on("mousedown", ".mSwipe-prev", methods.prev);
+				$this.on("click.mSwipe", ".mSwipe-next", methods.next);
+				$this.on("click.mSwipe", ".mSwipe-prev", methods.prev);
+
+				$(window).on("resize.mSwipe", throttleAndCleanup(self.initDimensions, function(){
+					$slideSled.removeClass("noTrans");
+				}));
 			};
+
 
 			var moveSlides = function(leftPosition){
-				$slideWrap.animate({"left" : leftPosition + "px"}, {
-					duration : settings.duration,
-					queue : false,
-					start : settings.onTransitionStart,
-					always : settings.onTransitionStart
-				});
+				console.log(settings.supportsCsstransitions, leftPosition);
+				if(settings.supportsCsstransitions){
+					$slideSled.css({"left" : leftPosition + "px"})
+				}else{
+					$slideSled.animate({"left" : leftPosition + "px"}, {
+						duration : settings.duration,
+						queue : false,
+						start : settings.onTransitionStart,
+						always : settings.onTransitionStart
+					});
+				}
 			};
+
 
 			var updateButtonState = function(){
-				if(currSlide == 1){
-					$prev.prop("disabled", true).addClass("disabled");
-				}else if($prev.is(":disabled")){
-					$prev.prop("disabled", false).removeClass("disabled");
-				}
-				if (currSlide >= totalSlides){
-					$next.prop("disabled", true).addClass("disabled");
-				}else if($next.is(":disabled")){
-					$next.prop("disabled", false).removeClass("disabled");
-				}
+				$this.toggleClass("firstSlide", (activeSlide == 0));
+				$prev.prop("disabled", (activeSlide == 0));
+
+				$this.toggleClass("lastSlide", (activeSlide == totalSlides));
+				$next.prop("disabled", (activeSlide == totalSlides));
 			};
 
+
+			
 			var methods = {
 				"prev" : function(){
-					console.log("prev", currSlide);
-					if(currSlide >= 1){
-						moveSlides(-$this.width() * (currSlide - 1));
-						currSlide--;
+					console.log("prev",activeSlide);
+					if(activeSlide > 0){
+						moveSlides(-$this.width() * (activeSlide-1));
+						activeSlide--;
 						updateButtonState();
 					}
 				},
 				"next" : function(){
-					console.log("next", currSlide);
-					if(currSlide < totalSlides){
-						moveSlides(-$this.width() * (currSlide));
-						currSlide++;
+					console.log("next",activeSlide);
+					if(activeSlide < totalSlides){
+						moveSlides(-$this.width() * (activeSlide+1));
+						activeSlide++;
+						updateButtonState();
 					}
 				}
 			};
 
 
-			initDimensions();
+			self.initDimensions();
 			updateButtonState();
 			bindEvents();
 		});
 
 	}
 
-})(jQuery);
+})(jQuery, window.Modernizr);
